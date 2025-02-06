d3f1671a016a978421fed6c22ef9b73074fd268c
Tarn Alcock <tarn.alcock@esgglobal.com>
2025-02-06 12:30:29 +0000

    add pets

--- Tables/dbo.NewTable.sql
+++ Tables/dbo.NewTable.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[NewTable]
 (
 [ID] [int] NULL,
-[Name] [varchar] (50) COLLATE Latin1_General_CI_AS NULL
+[Name] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
+[HasPets] [bit] NULL
 ) ON [PRIMARY]
 GO
