6114ef5e7b11b1764b850c7a9e2ace40d80fe077
Tarn Alcock <tarn.alcock@esgglobal.com>
2025-02-06 11:51:48 +0000

    Add phone number column

--- Tables/dbo.NewTable.sql
+++ Tables/dbo.NewTable.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[NewTable]
 (
 [ID] [int] NULL,
-[Name] [varchar] (50) COLLATE Latin1_General_CI_AS NULL
+[Name] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
+[Phone] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
